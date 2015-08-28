1a7296060ebc4b675af9f884a9aa01cf75285acb
kk <asdasd@asdasd.aa>
2015-08-28 11:49:24 +0300

    First commit

---
+++ pillax/common/Debug.php
@@ -0,0 +1,28 @@
+<?php 
+
+namespace Pillax;
+
+class Debug
+{
+	public static function dump($arr, $title='')
+	{
+		if(php_sapi_name() == 'cli')
+		{
+			htmlentities(print_r($arr));
+			echo "\r\n";
+		}
+		else
+		{
+			echo '
+			<div style="border: 1px solid red; background-color: #FFF0F0; padding: 5px;">
+				<pre>
+				' . (($title != '') ? '<b style="color: red;">' . $title . '</b><br />' : '');
+
+			htmlentities(print_r($arr));
+
+			echo '
+				</pre>
+			</div>';
+		}
+	}
+}
